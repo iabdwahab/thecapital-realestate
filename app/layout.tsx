763a4fc2a96bd7f86a828e27137d669c5d396d11
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-10-23 04:17:52 +0300

    Init Next.js with style guides.

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import { Cairo } from 'next/font/google';
+import './globals.css';
+
+const cairo = Cairo({
+  variable: '--font-cairo',
+  weight: ['300', '400', '600', '700', '800', '900'],
+  subsets: ['arabic'],
+});
+
+export const metadata: Metadata = {
+  title: 'ذا كابيتال - The Capital',
+  description: `تأسست ذا كابيتال برؤية طموحة تهدف إلى إعادة تعريف مفهوم الاستثمار العقاري عبر تقديم نماذج ومنتجات استثمارية مبتكرة وموثوقة، تمنح الأفراد والمستثمرين فرصًا عملية وآمنة للدخول في القطاع العقاري.`,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ar" dir="rtl">
+      <body className={` ${cairo.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-hero gradient-bg-inside text-white py-4">السلام عليكم!</h1>
+    </main>
+  );
+}
