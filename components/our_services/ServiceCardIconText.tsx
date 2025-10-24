8b91adf509a7576efc10d7f055eed347091793a0
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-10-24 07:19:10 +0300

    Implementing ServiceCardIconText.

--- components/our_services/ServiceCardIconText.tsx
+++ components/our_services/ServiceCardIconText.tsx
@@ -2,9 +2,17 @@ import Image from "next/image";
 
 export default function ServiceCardIconText() {
   return (
-    <div className="border-secondary-light bg-background rounded-card-radius flex flex-col items-center gap-2 border p-6">
-      <div>
-        <Image src="/icons/home-tree.svg" alt="Icon" width={50} height={50} />
+    <div className="border-secondary-light group hover:gradient-card-hover-light bg-background rounded-card-radius relative flex flex-col items-center gap-2 border p-6 transition duration-300">
+      <span className="rounded-card-radius group-hover:gradient-card-light-border absolute top-[5px] -z-10 h-full w-full transition duration-300"></span>
+
+      <div className="group-hover:gradient-bg-inside rounded-card-radius p-3">
+        <Image
+          src="/icons/home-tree.svg"
+          alt="Icon"
+          width={50}
+          height={50}
+          className="group-hover:white-svg transition duration-100"
+        />
       </div>
       <h3 className="text-body-large text-primary">تطوير الوحدات السكنية</h3>
     </div>
