e1857340ad1871f0fe177a587802da9778821cc6
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-10-24 12:10:43 +0300

    Adding Background Image for OurServicesSection.

--- components/home_page/OurServicesSection.tsx
+++ components/home_page/OurServicesSection.tsx
@@ -3,19 +3,21 @@ import ServiceCardIconText from "../our_services/ServiceCardIconText";
 
 export default function OurServicesSection() {
   return (
-    <section className="py-section-y-padding container">
-      <SectionTitleDescription
-        title="خدماتنا"
-        description="نقدم مجموعة شاملة من الخدمات العقارية المتكاملة لتلبية احتياجاتكم"
-      />
+    <section className="py-section-y-padding bg-[url('/services/background-shape.png')] bg-cover">
+      <div className="container">
+        <SectionTitleDescription
+          title="خدماتنا"
+          description="نقدم مجموعة شاملة من الخدمات العقارية المتكاملة لتلبية احتياجاتكم"
+        />
 
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-        <ServiceCardIconText />
-        <ServiceCardIconText />
-        <ServiceCardIconText />
-        <ServiceCardIconText />
-        <ServiceCardIconText />
-        <ServiceCardIconText />
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+          <ServiceCardIconText />
+          <ServiceCardIconText />
+          <ServiceCardIconText />
+          <ServiceCardIconText />
+          <ServiceCardIconText />
+          <ServiceCardIconText />
+        </div>
       </div>
     </section>
   );
