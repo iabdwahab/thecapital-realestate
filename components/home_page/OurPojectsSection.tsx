09aba9e5976d78f8aefd1fae9d53cb8191f4f8dc
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-10-25 08:11:57 +0300

    Update OurPojectsSection.tsx

--- components/home_page/OurPojectsSection.tsx
+++ components/home_page/OurPojectsSection.tsx
@@ -15,6 +15,8 @@ export default function OurPojectsSection() {
         Hey, this element is for mobile and Desktop.
         Desktop will use grid layout, and mobile will use horizontal scroll.
         You can see both implementations below, just uncomment the one you want to use.
+
+        I combined them into one div to reduce code duplication in html source.
       */}
       <div className="max-md:scrollbar-hide gap-4 max-md:flex max-md:overflow-x-auto max-md:px-4 max-md:py-4 md:container md:grid md:grid-cols-2 lg:grid-cols-3">
         <ProjectCardImageText className="max-md:w-[calc(100vw-32px-15px)] max-md:shrink-0" />
