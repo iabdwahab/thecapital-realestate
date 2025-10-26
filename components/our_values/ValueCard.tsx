0c06d8d81437254eb46879a0a71ac3ae9f4d6dcb
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-10-26 10:26:56 +0300

    Fixing Error.

--- components/our_values/ValueCard.tsx
+++ components/our_values/ValueCard.tsx
@@ -7,7 +7,7 @@ export default function ValueCard() {
       tabIndex={0}
       className="bg-background group border-secondary-light max-lg:focus:bg-primary hover:bg-primary rounded-card-radius border p-6 transition duration-300"
     >
-      <IconGradientCard />
+      <IconGradientCard icon="" />
       <div className="text-primary">
         <h3 className="text-body-large max-lg:group-focus:text-background group-hover:text-background mb-1">
           الشفافية
