07e7cd18fb5b82ce27036104a168cb41f631c6fe
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-10-26 17:47:37 +0300

    Update transition duration classes in ServiceCardIconText.

--- components/our_services/ServiceCardIconText.tsx
+++ components/our_services/ServiceCardIconText.tsx
@@ -10,10 +10,10 @@ export default function ServiceCardIconText({ icon, title }: Props) {
   return (
     <div
       tabIndex={0}
-      className="border-secondary-light group hover:gradient-card-hover-light max-lg:focus:gradient-card-hover-light bg-background rounded-card-radius relative flex flex-col items-center gap-2 border p-6 transition duration-400"
+      className="border-secondary-light group hover:gradient-card-hover-light max-lg:focus:gradient-card-hover-light bg-background rounded-card-radius duration-hover-normal relative flex flex-col items-center gap-2 border p-6 transition"
     >
       <GradientCardBorder />
-      <div className="group-hover:gradient-bg-inside max-lg:group-focus:gradient-bg-inside rounded-card-radius p-3">
+      <div className="group-hover:gradient-bg-inside duration-hover-normal max-lg:group-focus:gradient-bg-inside rounded-card-radius p-3 transition">
         <Image
           src={icon ? icon : `/icons/home-tree.svg`}
           alt="Icon"
