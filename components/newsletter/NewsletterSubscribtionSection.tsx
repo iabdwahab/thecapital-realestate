4376e99a6fa051f36de64779318601fa81a8169b
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-10-29 23:33:30 +0300

    Update newsletter section background style.

--- components/newsletter/NewsletterSubscribtionSection.tsx
+++ components/newsletter/NewsletterSubscribtionSection.tsx
@@ -6,7 +6,7 @@ export default async function NewsletterSubscribtionSection() {
   const sectionHeadingData: SectionHeadingData | null = await getSectionHeading("newsletter");
 
   return (
-    <section className="py-section-y-padding text-background bg-primary">
+    <section className="py-section-y-padding text-background from-primary to-primary via-primary-light bg-linear-to-b">
       <div className="container">
         <div className="mb-10">
           <h2 className="text-heading mb-3">
