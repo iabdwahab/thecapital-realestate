65f32cf8e1f88ea7276e839e1e4988d2981a54dc
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-10-29 17:42:47 +0300

    Implementing StartInvestmentSection.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import SectionTitleDescription from "@/components/global/SectionTitleDescription";
 import HeroSection from "@/components/home_page/HeroSection";
 import OurPojectsSection from "@/components/home_page/OurPojectsSection";
 import OurServicesSection from "@/components/home_page/OurServicesSection";
 import SummaryValuesSection from "@/components/home_page/summary_valeus_section/SummaryValuesSection";
 import WhyUsSection from "@/components/home_page/WhyUsSection";
+import StartInvestmentSection from "@/components/start_investment_section/StartInvestmentSection";
 
 export default function Home() {
   return (
@@ -13,6 +13,7 @@ export default function Home() {
       <OurServicesSection />
       <OurPojectsSection />
       <WhyUsSection />
+      <StartInvestmentSection />
     </>
   );
 }
---
+++ components/start_investment_section/StartInvestmentSection.tsx
@@ -0,0 +1,49 @@
+import { SectionHeadingData } from "@/types/pageHeading";
+import { getSectionHeading } from "@/utils/getSectionHeading";
+import Link from "next/link";
+
+export default async function StartInvestmentSection() {
+  const sectionHeadingData: SectionHeadingData | null = await getSectionHeading("start_investment");
+
+  return (
+    <section className="py-section-y-padding bg-primary text-background text-center">
+      <div className="container">
+        <div className="mb-12">
+          <h2 className="text-heading mb-3">
+            {sectionHeadingData?.title || "ابدأ رحلتك الاستثمارية معنا اليوم"}
+          </h2>
+          <p className="text-body-large">
+            {sectionHeadingData?.description ||
+              "استثمر في وحدات فندقية ، بمبالغ رمزية ، وعوائد مجزية .. فريقنا جاهز لمساعدتك في الاطلاع على الفرص واتخاذ قرار الاستثمار."}
+          </p>
+        </div>
+
+        <div className="flex items-center justify-center gap-4">
+          <Link
+            href="/projects"
+            className="btn-primary text-primary flex w-fit items-center gap-3 bg-white hover:bg-white/90 active:bg-white/80"
+          >
+            <span>تصفح المشاريع</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+              />
+            </svg>
+          </Link>
+          <Link href="/contact" className="btn-primary border-secondary-light border">
+            <span>تواصل معنا</span>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}
