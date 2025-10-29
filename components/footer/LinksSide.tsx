92f2c7f14691f3917ff3aa330ce009ded2947636
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-10-29 17:50:35 +0300

    Replace static footer links with dynamic navLinks array.

--- components/footer/LinksSide.tsx
+++ components/footer/LinksSide.tsx
@@ -4,29 +4,49 @@ import Link from "next/link";
 export default async function LinksSide() {
   const servicesList = await getServices();
 
+  const navLinks = [
+    {
+      id: 1,
+      title: "الصفحة الرئيسية",
+      href: "/",
+    },
+    {
+      id: 2,
+      title: "عن ذا كابيتال",
+      href: "/about",
+    },
+    {
+      id: 3,
+      title: "خدماتنا",
+      href: "/services",
+    },
+    {
+      id: 4,
+      title: "مشاريعنا",
+      href: "/projects",
+    },
+    {
+      id: 5,
+      title: "أخبارنا",
+      href: "/news",
+    },
+    {
+      id: 7,
+      title: "تواصل معنا",
+      href: "/contact",
+    },
+  ];
+
   return (
     <div className="grid gap-12 md:grid-cols-3">
       <div>
         <h3 className="text-sub-heading text-background mb-6">روابط سريعة</h3>
         <ul className="text-body-small flex flex-col gap-3 text-[#CCD1D2]">
-          <li>
-            <Link href="#">الرئيسية</Link>
-          </li>
-          <li>
-            <Link href="#">الرئيسية</Link>
-          </li>
-          <li>
-            <Link href="#">الرئيسية</Link>
-          </li>
-          <li>
-            <Link href="#">الرئيسية</Link>
-          </li>
-          <li>
-            <Link href="#">الرئيسية</Link>
-          </li>
-          <li>
-            <Link href="#">الرئيسية</Link>
-          </li>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <Link href={link.href}>{link.title}</Link>
+            </li>
+          ))}
         </ul>
       </div>
       <div>
