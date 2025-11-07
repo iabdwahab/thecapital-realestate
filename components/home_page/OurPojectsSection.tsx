3266e107fb1fde086a90c2c06ae3a37a3bfc4617
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-11-07 04:23:04 +0200

    Update OurPojectsSection layout of homepage.

--- components/home_page/OurPojectsSection.tsx
+++ components/home_page/OurPojectsSection.tsx
@@ -3,6 +3,7 @@ import SectionTitleDescription from "../global/SectionTitleDescription";
 import ProjectCardImageText from "../our_projects/ProjectCardImageText";
 import { getSectionHeading } from "@/utils/getSectionHeading";
 import { SectionHeadingData } from "@/types/sectionHeading";
+import ButtonPrimaryArrow from "../global/ButtonPrimaryArrow";
 
 export default async function OurPojectsSection() {
   const sectionHeadingData: SectionHeadingData | null = await getSectionHeading("projects");
@@ -20,6 +21,20 @@ export default async function OurPojectsSection() {
         />
       </div>
 
+      <div className="container grid gap-4 md:grid-cols-2">
+        {projectsList.slice(0, 2).map((project) => {
+          return <ProjectCardImageText key={project.id} data={project} />;
+        })}
+      </div>
+
+      <div className="container mt-8 flex justify-center">
+        <ButtonPrimaryArrow
+          href="/projects"
+          text="اكتشف جميع مشاريعنا"
+          className="btn-primary block"
+        />
+      </div>
+
       {/*
         Hey, this element is for mobile and Desktop.
         Desktop will use grid layout, and mobile will use horizontal scroll.
@@ -27,7 +42,7 @@ export default async function OurPojectsSection() {
 
         I combined them into one div to reduce code duplication in html source.
       */}
-      <div className="max-md:scrollbar-hide gap-4 max-md:flex max-md:overflow-x-auto max-md:px-4 max-md:py-4 md:container md:grid md:grid-cols-2 lg:grid-cols-3">
+      {/* <div className="max-md:scrollbar-hide gap-4 max-md:flex max-md:overflow-x-auto max-md:px-4 max-md:py-4 md:container md:grid md:grid-cols-2 lg:grid-cols-3">
         {projectsList.map((project) => {
           return (
             <ProjectCardImageText
@@ -37,7 +52,7 @@ export default async function OurPojectsSection() {
             />
           );
         })}
-      </div>
+      </div> */}
 
       {/* Mobile Slider */}
       {/* <div className="scrollbar-hide flex gap-4 overflow-x-auto px-4 md:hidden">
