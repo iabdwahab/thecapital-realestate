162f0189c6a38620354792ac4127837c03783fa4
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-11-09 18:22:13 +0200

    Update config file.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   /* config options here */
   images: {
     remotePatterns: [new URL(process.env.NEXT_PUBLIC_WORDPRESS_URL + "/**" || "")],
+    unoptimized: true,
   },
   experimental: {
     turbopackFileSystemCacheForDev: true,
