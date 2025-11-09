f0ba6cad39469fac909236da7a215d949b584d00
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-11-10 01:46:20 +0200

    Highlight active nav link in header components.

--- components/header/HeaderNav.tsx
+++ components/header/HeaderNav.tsx
@@ -1,17 +1,27 @@
+"use client";
+
 import { NavLink } from "@/types/headerInfo";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface Props {
   navLinks: NavLink[];
 }
 
 export default function HeaderNav({ navLinks }: Props) {
+  const currentPath = usePathname();
+
   return (
     <nav className="header-breakpoint:block hidden">
       <ul className="flex items-center gap-6">
         {navLinks.map((link, index) => (
           <li key={index}>
-            <Link href={link.href}>{link.text}</Link>
+            <Link
+              href={link.href}
+              className={`${currentPath === link.href ? "border-b font-semibold" : ""}`}
+            >
+              {link.text}
+            </Link>
           </li>
         ))}
       </ul>
