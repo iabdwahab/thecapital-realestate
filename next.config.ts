d07e3e809acc45d909114163572fe927d48722cb
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-11-10 19:58:27 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [new URL(process.env.NEXT_PUBLIC_WORDPRESS_URL + "/**" || "")],
-    // unoptimized: true,
+    unoptimized: true,
   },
   experimental: {
     turbopackFileSystemCacheForDev: true,
