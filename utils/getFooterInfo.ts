c9aca52eef4711a58e38923a0ef16f8ee205935d
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-11-10 02:16:02 +0200

    Add revalidation to footer info fetch request.

--- utils/getFooterInfo.ts
+++ utils/getFooterInfo.ts
@@ -2,7 +2,9 @@ import { FooterInfoFetchedObject } from "@/types/footerInfo";
 
 export async function getFooterInfo() {
   try {
-    const reasonsRes = await fetch(`${process.env.NEXT_PUBLIC_ACF_API_URL}/posts/201`);
+    const reasonsRes = await fetch(`${process.env.NEXT_PUBLIC_ACF_API_URL}/posts/201`, {
+      next: { revalidate: 60 },
+    });
 
     const data: FooterInfoFetchedObject = await reasonsRes.json();
 
