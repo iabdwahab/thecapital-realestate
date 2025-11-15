1dc941b165e8fffe4623630da1b3d2d45c67a0cd
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-11-15 17:53:51 +0200

    Add react-hook-form to LoginForm for validation.
    Hey, It is client validation as this is not important to know the email and password.
    
    user must type exactly email and password in wp admin panel.

--- components/auth/login/LoginForm.tsx
+++ components/auth/login/LoginForm.tsx
@@ -1,11 +1,34 @@
 "use client";
 
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+
+interface LoginFormInputs {
+  email: string;
+  password: string;
+}
 
 export default function LoginForm() {
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    clearErrors,
+    formState: { errors },
+  } = useForm<LoginFormInputs>();
+
+  function onSubmit(data: LoginFormInputs) {
+    if (data.email === "admin@thecapital.sa" && data.password === "adminpassword") {
+      router.push("http://wp.thecapital.sa/wp-admin/");
+    } else {
+      setError("root", { message: "بيانات تسجيل الدخول غير صحيحة." });
+    }
+  }
   return (
     <form
-      onSubmit={(e) => e.preventDefault()}
+      onSubmit={handleSubmit(onSubmit)}
       className="rounded-card-radius border-primary w-full max-w-3xl border px-8 py-8 shadow-sm md:px-16"
     >
       <div className="mb-3 flex flex-col gap-3">
@@ -13,12 +36,21 @@ export default function LoginForm() {
           البريد الإلكتروني
         </label>
         <input
-          type="email"
-          name="email"
+          type="text"
           id="email"
+          {...register("email", {
+            required: "يجب إدخال بريد إلكتروني صالح.",
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: "بريد إلكتروني غير صالح.",
+            },
+            onChange: () => clearErrors("root"),
+          })}
           placeholder="أدخل بريدك الإلكتروني"
           className="text-secondary-light bg-background/5 outline-primary-light text-body-small border-secondary-light rounded-xl border p-3"
         />
+
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
       </div>
 
       <div className="mb-16 flex flex-col gap-3">
@@ -27,14 +59,24 @@ export default function LoginForm() {
         </label>
         <input
           type="password"
-          name="password"
+          {...register("password", {
+            required: "يجب إدخال كلمة المرور.",
+            minLength: { value: 6, message: "يجب أن تكون كلمة المرور أكثر من 6 حروف." },
+            onChange: () => clearErrors("root"),
+          })}
           id="password"
           placeholder="أدخل كلمة المرور"
           className="text-secondary-light bg-background/5 outline-primary-light text-body-small border-secondary-light rounded-xl border p-3"
         />
+
+        {errors.password && <p className="text-red-500">{errors.password.message}</p>}
       </div>
 
       <button className="btn-secondary mb-10 w-full">تسجيل الدخول</button>
+
+      <p className={`text-center text-red-500 ${errors?.root?.message ? "" : "invisible"}`}>
+        {errors?.root?.message || "."}
+      </p>
 
       {/* <p className="text-body-large text-center">
         ليس لديك حساب؟{" "}
