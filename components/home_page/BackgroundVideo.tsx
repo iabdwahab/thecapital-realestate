eaa43934a007bf3b21bed43a0aa31d54384a5c7b
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-11-27 10:20:04 +0200

    Hide controls of the bg video in herosection.

--- components/home_page/BackgroundVideo.tsx
+++ components/home_page/BackgroundVideo.tsx
@@ -2,7 +2,13 @@
 
 export default function BackgroundVideo() {
   return (
-    <video autoPlay muted loop className="absolute inset-0 -z-10 h-full w-full object-cover">
+    <video
+      autoPlay
+      controls={false}
+      muted
+      loop
+      className="absolute inset-0 -z-10 h-full w-full object-cover"
+    >
       <source src="/hero-video.mp4" type="video/mp4" />
     </video>
   );
